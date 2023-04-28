721d8e450381b9a7a8bf4dfa84d82da3917f9fe3
hajar <hajar.nhaila.97@gmail.com>
2023-04-28 09:10:18 +0100

    Le fichier code et le fichier Readme sont crées

---
+++ code.cpp
@@ -0,0 +1,15 @@
+// Compte.cpp : définit le point d'entrée pour l'application console.
+// 
+#include "stdafx.h"
+ 
+ int _tmain(int argc, _TCHAR* argv[]) 
+ { 
+	int i = 1; 
+
+	while ( i <= 10) 
+ { 
+	printf("N = %d", i); 
+	i = i + 1; 
+ } 
+ return 0; 
+ }
